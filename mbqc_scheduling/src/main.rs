3110fdb9037463c5ceae2ca053b3a2ea49dd5118
Jannis Ruh <ruh.j81@gmail.com>
2024-01-16 09:04:04 +1100

    Get more test files

--- mbqc_scheduling/src/main.rs
+++ mbqc_scheduling/src/main.rs
@@ -3,57 +3,63 @@ use mbqc_scheduling::{
     interface,
     probabilistic::AcceptFunc,
 };
+use utils::serialization::Dynamic;
 
 mod cli;
 
-fn main() {
-    let Args {
-        spacial_graph,
-        spacial_graph_format,
-        dependency_graph,
-        dependency_graph_format,
-        paths,
-        paths_format,
-        search,
-        nthreads,
-        probabilistic,
-        task_bound,
-        debug,
-    } = cli::parse();
-
-    interface::run_serialized(
-        (spacial_graph, &spacial_graph_format),
-        (dependency_graph, &dependency_graph_format),
-        search,
-        nthreads,
-        // probabilistic.then_some(AcceptFunc::Standard),
-        // TODO: make proper tests for this sanity test here (just checking here whether
-        // there's no typo in AcceptFunc::CreateFunc)
-        task_bound,
-        probabilistic.then_some(create_accept_func()),
-        debug,
-        (paths, &paths_format),
-    )
-    .expect("path search failed: ")
+fn transform() {
+    use pauli_tracker::{
+        collection::{
+            Iterable,
+            Map,
+        },
+        pauli::PauliStack,
+        tracker::frames::{
+            dependency_graph,
+            Frames,
+        },
+    };
+    let serde = Dynamic::SerdeJson;
+    let frames: Frames<Map<PauliStack<Vec<bool>>>> = serde
+        .read_file("../test_files/cluster/fourier_5_ooooo_frames.json")
+        .unwrap();
+    serde
+        .write_file(
+            "../test_files/fourier_5o_dependency.json",
+            &dependency_graph::create_dependency_graph(
+                frames.as_storage().iter_pairs(),
+                &[4, 5, 6, 7, 8, 9, 10, 11, 3, 13, 14, 15, 16, 17, 2, 19, 20, 21, 1, 23],
+            ),
+        )
+        .unwrap();
 }
 
-// cf. above
-fn create_accept_func() -> AcceptFunc {
-    AcceptFunc::ParametrizedBasic {
-        weights: mbqc_scheduling::probabilistic::Weights {
-            bound_best_mem: 1.,
-            last_max_mem: 0.,
-            last_cur_mem: 0.,
-            cur_mem: 1.,
-            num_measure_nodes: 1.,
-            num_total_nodes: 8.5e-2,
-        },
-        shifts: mbqc_scheduling::probabilistic::Shifts {
-            upper_mem: 1.,
-            cur_mem: 1.,
-            time: 1e-3,
-            num_measure_nodes: 1.,
-            num_total_nodes: 8.5e-2,
-        },
-    }
+fn main() {
+    transform()
+
+    // let Args {
+    //     spacial_graph,
+    //     spacial_graph_format,
+    //     dependency_graph,
+    //     dependency_graph_format,
+    //     paths,
+    //     paths_format,
+    //     search,
+    //     nthreads,
+    //     probabilistic,
+    //     task_bound,
+    //     debug,
+    // } = cli::parse();
+
+    // interface::run_serialized(
+    //     (spacial_graph, &spacial_graph_format),
+    //     (dependency_graph, &dependency_graph_format),
+    //     search,
+    //     nthreads,
+    //     task_bound,
+    //     probabilistic.then_some(AcceptFunc::BuiltinBasic),
+    //     debug,
+    //     (paths, &paths_format),
+    // )
+    // .expect("path search failed: ")
 }
